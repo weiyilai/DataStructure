fde4e2a626dd5eb46374f6c196c7b1e72b96334c
joy777park <joy777park@gmail.com>
2023-11-07 17:55:05 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] result = new[]
+{
+    1,4,3,6,9,11,8,36
+};
+
+List<int> list = result.ToList();
+list.Insert(2, 23);
+
+Console.WriteLine(string.Join(", ", list));
+Console.ReadLine();
